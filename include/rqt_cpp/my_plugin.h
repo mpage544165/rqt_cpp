95acd7a15b8bd857b1a16755a0291fa106e86c9b
mpage544165 <55064915+mpage544165@users.noreply.github.com>
2020-04-09 19:55:07 -0700

    Add files via upload

---
+++ include/rqt_cpp/my_plugin.h
@@ -0,0 +1,42 @@
+
+#ifndef RQT_CPP_MY_PLUGIN_H
+#define RQT_CPP_MY_PLUGIN_H
+
+#include <rqt_gui_cpp/plugin.h>
+#include <rqt_cpp/ui_my_plugin.h>
+#include <QWidget>
+#include <QImage>
+
+#include <image_transport/image_transport.h>
+#include <sensor_msgs/Image.h>
+
+namespace rqt_cpp
+{
+
+class MyPlugin
+  : public rqt_gui_cpp::Plugin
+  {
+    Q_OBJECT
+  public:
+    MyPlugin();
+    virtual void initPlugin(qt_gui_cpp::PluginContext& context);
+    virtual void shutdownPlugin();
+    virtual void saveSettings(qt_gui_cpp::Settings& plugin_settings,
+        qt_gui_cpp::Settings& instance_settings) const;
+    virtual void restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+        const qt_gui_cpp::Settings& instance_settings);
+
+    virtual void imageCallback(const sensor_msgs::Image::ConstPtr& msg);
+
+    // Comment in to signal that the plugin has a way to configure it
+    // bool hasConfiguration() const;
+    // void triggerConfiguration();
+  private:
+
+    Ui::MyPluginWidget ui_;
+    QWidget* widget_;
+
+    image_transport::Subscriber imSub;
+  };
+}  // namespace rqt_cpp
+#endif  // RQT_CPP_MY_PLUGIN_H
---
+++ src/rqt_cpp/my_plugin.cpp
@@ -0,0 +1,76 @@
+
+#include "rqt_cpp/my_plugin.h"
+#include <pluginlib/class_list_macros.h>
+#include <QStringList>
+
+#include <cv_bridge/cv_bridge.h>
+#include <opencv2/imgproc/imgproc.hpp>
+
+namespace rqt_cpp
+{
+
+  MyPlugin::MyPlugin()
+    : rqt_gui_cpp::Plugin()
+    , widget_(0)
+  {
+  // Constructor is called first before initPlugin function, needless to say.
+
+  // give QObjects reasonable names
+    setObjectName("MyPlugin");
+
+    
+  }
+
+  void MyPlugin::initPlugin(qt_gui_cpp::PluginContext& context)
+  {
+    // access standalone command line arguments
+    QStringList argv = context.argv();
+    // create QWidget
+    widget_ = new QWidget();
+    // extend the widget with all attributes and children from UI file
+    ui_.setupUi(widget_);
+    // add widget to the user interface
+    context.addWidget(widget_);
+
+    image_transport::ImageTransport it(getNodeHandle());
+    imSub = it.subscribe("/cvImage", 1, &MyPlugin::imageCallback, this);
+  }
+
+  void MyPlugin::shutdownPlugin()
+  {
+    // unregister all publishers here
+    imSub.shutdown();
+  }
+
+  void MyPlugin::saveSettings(qt_gui_cpp::Settings& plugin_settings,
+      qt_gui_cpp::Settings& instance_settings) const
+  {
+    // instance_settings.setValue(k, v)
+  }
+
+  void MyPlugin::restoreSettings(const qt_gui_cpp::Settings& plugin_settings,
+      const qt_gui_cpp::Settings& instance_settings)
+  {
+    // v = instance_settings.value(k)
+  }
+
+  /*bool hasConfiguration() const
+  {
+    return true;
+  }
+
+  void triggerConfiguration()
+  {
+    // Usually used to open a dialog to offer the user a set of configuration
+  }*/
+
+  void MyPlugin::imageCallback(const sensor_msgs::Image::ConstPtr& msg){
+    cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::RGB8);
+    QImage image(cv_ptr->image.data, cv_ptr->image.cols, cv_ptr->image.rows, cv_ptr->image.step[0], QImage::Format_RGB888);
+    ui_.label->setPixmap(QPixmap::fromImage(image));
+  }
+
+}  // namespace rqt_example_cpp
+PLUGINLIB_DECLARE_CLASS(rqt_cpp, MyPlugin, rqt_cpp::MyPlugin, rqt_gui_cpp::Plugin)
+
+
